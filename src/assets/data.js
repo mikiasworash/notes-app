84a7f69f7b42f7af4a63f3cd4bdbf5ddc3338ca8
Mikias <mikias.worash.a@gmail.com>
2022-04-22 19:07:54 +0300

    styling added

--- src/assets/data.js
+++ src/assets/data.js
@@ -1,17 +1,17 @@
 let notes = [
   {
     id: 1,
-    body: "Todays Agenda\n\n- Walk Dog\n- Feed fish\n- Play basketball\n- Eat a salad",
+    body: "Todays Agenda\n\n- Wakeup\n- Kick Ass\n- Repeat",
     updated: "2021-07-14T13:49:02.078653Z",
   },
   {
     id: 2,
-    body: "Bob from bar down the \n\n- Take out trash\n- Eat food",
+    body: "Tomorrow's Agenda \n\n- Wakeup\n- Kick Ass\n- Repeat",
     updated: "2021-07-13T20:43:18.550058Z",
   },
   {
     id: 3,
-    body: "Wash car",
+    body: "The day after Tomorrow's Agenda \n\n- Wakeup\n- Kick Ass\n- Repeat",
     updated: "2021-07-13T19:46:12.187306Z",
   },
 ];
--- src/pages/NotePage.js
+++ src/pages/NotePage.js
@@ -1,13 +1,20 @@
 import React from "react";
 import notes from "../assets/data";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
+import { ReactComponent as ArrowLeft } from "../assets/arrow-left.svg";
 
 const NotePage = () => {
   let noteId = useParams().id;
   let note = notes.find((note) => note.id == noteId);
   return (
     <div className="note">
-      <div className="note-header"></div>
+      <div className="note-header">
+        <h3>
+          <Link to="/">
+            <ArrowLeft />
+          </Link>
+        </h3>
+      </div>
       <textarea value={note?.body}></textarea>
     </div>
   );
